e3e64988458c44bf805225b4a032cbe67d6c149c
Ryan Cox <ryan_cox@byu.edu>
2013-10-18 15:15:57 -0600

    reverted accidental copy of old file and moved in the correct oom_notifierd.c

--- oom_notifierd/oom_notifierd.c
+++ oom_notifierd/oom_notifierd.c
@@ -1,35 +1,49 @@
-/* This program notifies a user when the oom-killer kills
- * a process in that user's cgroup.  It will only allow one
- * copy of itself to run and exits if another program of the
- * same name by the same user exists.  It terminates if the
- * user has no other processes except this one within a
- * certain amount of time.  The notification is done to the
- * most recently used tty.  This was not coded in a portable
- * fashion but should be relatively easy to port to other
- * Unix-like systems.
+/* 
+ * Description:
+ * This program notifies a user when the oom-killer kills a process in that
+ * user's cgroup.  It will only allow one copy of itself to run and exits if
+ * another program of the same name by the same user exists.  It terminates
+ * if the user has no other processes except this one within a certain amount
+ * of time.  The notification is done to all of a user's ttys.  This was not
+ * coded in a portable fashion so minor changes may be necessary for
+ * different kernels/distros.
+ * See also: https://www.kernel.org/doc/Documentation/cgroups/memory.txt
  *
- * Author:    Ryan Cox <ryan_cox@byu.edu>
+ * Bugs:  (TODO)
+ *     - Check for other user procs only checks if itself is not in the list.
+ *     		-Needs to check if any of the others are the same type of prog
+ *     		but monitoring a different directory
+ *
+ * Assumptions:
+ *     - You want to notify all of a user's TTYs
+ *     - TTYs are in /dev/pts/
+ *         - Change DEV_TTY_GLOB if not
+ *
+ * Author:   Ryan Cox <ryan_cox@byu.edu>
+ *
+ * Copyright (C) 2013 Brigham Young University
  * 
- * Copyright (C) 2013, Brigham Young University
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
- * to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
- *
  */
+
+#include <libgen.h>
 #include <sys/eventfd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -39,52 +53,18 @@
 #include <stdint.h>             /* Definition of uint64_t */
 #include <stdio.h>
 #include <string.h>
-#include <pwd.h>
 #include <errno.h>
 #include <glob.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define handle_error(msg) \
     do { perror(msg); exit(1); } while (0)
 
 
-#define ERROR_MSG_TO_USER "\nYou exceeded your memory limit on this host. The kernel invoked the oom-killer which killed a process of yours to free up memory. No further action is required.\nRun 'loginlimits' to see the current limits.\n\n"
-
-
-FILE *debugfd;
-
-int getUsernameFromUid(uid_t uid, char *username) {
-	struct passwd pwd;
-	struct passwd *result;
-	char *buf;
-	size_t bufsize;
-	int s;
-
-	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
-	if (bufsize == -1)          /* Value was indeterminate */
-		bufsize = 16384;        /* Should be more than enough */
-
-	buf = malloc(bufsize);
-	if (buf == NULL) {
-		perror("malloc");
-		return 0;
-	}
-
-	s = getpwuid_r(uid, &pwd, buf, bufsize, &result);
-	if (result == NULL) {
-		if (s == 0) {
-			/* DEBUG("Not found\n");*/
-		} else {
-			errno = s;
-			perror("getpwnam_r");
-		}
-		return 0;
-	}
-
-	strcpy(username, pwd.pw_name);
-	return 1;
-
-}
+#define ERROR_MSG_TO_USER "You exceeded your memory limit on this host. The kernel invoked the oom-killer which killed a process of yours to free up memory. No further action is required.\nRun 'loginlimits' to see the current limits.\a"
+#define DEV_TTY_GLOB "/dev/pts/*"
+#define TIMEOUT_SECONDS 60
 
 void daemonize() {
 	chdir("/");
@@ -100,7 +80,6 @@ int walkUserProcesses(int (*cb)(char *, uid_t, pid_t, pid_t, char *), char *arg1
 	int i, retval;
 	glob_t globbuf;
 	struct stat stat;
-	int fd;
 	char *bname;
 	char path_temp[40];
 	pid_t pid, examine_pid;
@@ -114,39 +93,54 @@ int walkUserProcesses(int (*cb)(char *, uid_t, pid_t, pid_t, char *), char *arg1
 		if(lstat(globbuf.gl_pathv[i], &stat)==0) {
 			if(stat.st_uid == uid) {
 				strncpy(path_temp, globbuf.gl_pathv[i], 40);
-				bname = basename(globbuf.gl_pathv[i]);
+				bname = (char *)basename(globbuf.gl_pathv[i]);
 				examine_pid = atol(bname);
-				if(retval = cb((char *)globbuf.gl_pathv[i], (uid_t)uid, (pid_t)pid, (pid_t)examine_pid, (char *)arg1)) {
+				if( (retval = cb((char *)globbuf.gl_pathv[i], (uid_t)uid, (pid_t)pid, (pid_t)examine_pid, (char *)arg1)) ) {
 					globfree(&globbuf);
 					return retval;
 				}
 			}
 		}
 	}
-
+		
 	globfree(&globbuf);
 	return 0;
 }
 
 int findDuplicate(char *progname, uid_t uid) {
+	char mycmdline[1024];
+	int fd, bytes, i;
+
+	fd = open("/proc/self/cmdline", O_RDONLY);
+	if(fd == -1)
+		handle_error("Couldn't open /proc/self/cmdline?!");
+	bytes = read(fd, mycmdline, 1024);
+	close(fd);
+	/* args are separated by \0. replace with space */
+	for(i = 0; i < bytes-1; i++)
+		if(mycmdline[i] == '\0')
+			mycmdline[i] = ' ';
+
 	int findDuplicate_cb(char *path, uid_t uid, pid_t mypid, pid_t examine_pid, char *progname) {
-		int fd;
-		char filename[255];
-		char cmdline[255];
+		int fd, bytes, i;
+		char filename[1024];
+		char cmdline[1024];
 		sprintf(filename, "%s/cmdline", path);
 		fd = open(filename, O_RDONLY);
 		if(fd != -1) {
-			read(fd, cmdline, 255);
-			if(strcmp(progname, cmdline)==0) {
-				if(mypid != examine_pid) {
-					return 1;
-				}
-			}
+			bytes = read(fd, cmdline, 1024);
 			close(fd);
+			for(i = 0; i < bytes-1; i++)
+				if(cmdline[i] == '\0')
+					cmdline[i] = ' ';
+			if(strcmp(progname, cmdline)==0) {
+				if(mypid != examine_pid)
+					return 1;
+			}
 		}
 		return 0;
 	}
-	return walkUserProcesses(&findDuplicate_cb, progname);
+	return walkUserProcesses(&findDuplicate_cb, mycmdline);
 }
 
 int userHasOtherProcesses(uid_t uid) {
@@ -158,47 +152,38 @@ int userHasOtherProcesses(uid_t uid) {
 	return retval;
 }
 
-int findActiveTty(uid_t uid, char *path) {
+void writeToTTY(char *path, char *msg) {
+	int ttyfd;
+	ssize_t s;
+
+	ttyfd = open(path, O_WRONLY);
+	s = write(ttyfd, msg, strlen(msg));
+	if(s != strlen(ERROR_MSG_TO_USER))
+		printf("non-fatal: couldn't write to terminal: %s", path);
+	close(ttyfd);
+}
+
+void writeToUserTTYs(uid_t uid) {
 	glob_t globbuf;
-	char most_recent_tty[255];
-	time_t most_recent_time;
 	struct stat stat;
 	int i;
+	char msg[1024];
+	time_t localtime;
+	char localtime_str[26];
 
-	most_recent_time = 0;
+	time(&localtime);
+	ctime_r(&localtime, localtime_str);
+	sprintf(msg, "\e[31m\n\n<<<<<<<<<<<<<<\n%s\n%s\n>>>>>>>>>>>>>>\e[0m\n\n", localtime_str, ERROR_MSG_TO_USER);
 	globbuf.gl_offs = 0;
-	glob("/dev/pts/*", GLOB_NOSORT, NULL, &globbuf);
+	glob(DEV_TTY_GLOB, GLOB_NOSORT, NULL, &globbuf);
 	for(i=0; i < globbuf.gl_pathc; i++) {
 		if(lstat(globbuf.gl_pathv[i], &stat)==0) {
 			if(stat.st_uid == uid) {
-				if(stat.st_mtime > most_recent_time) {
-					strncpy(most_recent_tty, globbuf.gl_pathv[i], 255);
-					most_recent_time = stat.st_mtime;
-				}
+				writeToTTY(globbuf.gl_pathv[i], msg);
 			}
 		}
 	}
 	globfree(&globbuf);
-	strncpy(path, most_recent_tty, 255);
-	return most_recent_time;
-}
-
-void writeToActiveTty(uid_t uid) {
-	char path[255];
-	int most_recent_time;
-	int ttyfd;
-	ssize_t s;
-	most_recent_time = findActiveTty(uid, path);
-	if(most_recent_time == 0)
-		return;
-	ttyfd = open(path, O_WRONLY);
-        if(ttyfd == -1) {
-		return;
-	}
-
-        s = write(ttyfd, ERROR_MSG_TO_USER, strlen(ERROR_MSG_TO_USER));
-	/* if it didn't write, there's not much we can do about it */
-	close(ttyfd);
 }
 
 int get_memcg_self_path(char *path) {
@@ -206,10 +191,8 @@ int get_memcg_self_path(char *path) {
 	int is_memory_cgroup_line, pos, retval = 0;
 	char *strtok_ptr, *str1, *token, line[512];
 	fp = fopen("/proc/self/cgroup", "r");
-	if(fp==NULL) {
-		printf("failed to open /proc/self/cgroup");
-		exit(1);
-	}
+	if(fp==NULL)
+		handle_error("failed to open /proc/self/cgroup");
 	while(fgets(line, 512, fp) != NULL) {
 		is_memory_cgroup_line = 0;
 		for (pos = 0, str1 = line; ; pos++, str1 = NULL) {
@@ -257,30 +240,76 @@ int get_memcg_mount_path(char *path) {
 	return retval;
 }
 
-int get_cgroup_path(char *memcg_path) {
-	char mount_path[256], self_path[256];
+char * get_cgroup_path() {
+	char *memcg_path;
+	char mount_path[1024], self_path[1024], path[1024];
 	if(!get_memcg_mount_path(mount_path))
-		return 1;
+		handle_error("Couldn't find memory cgroup mount point in /proc/mounts");
 	if(!get_memcg_self_path(self_path))
-		return 1;
-	sprintf(memcg_path, "%s%s", mount_path, self_path);
-	return 0;
+		handle_error("Couldn't find memory cgroup path in /proc/self/cgroup. Am I in a cgroup yet?");
+	memcg_path = (char *)malloc(sizeof(char) * (strlen(mount_path) + strlen(self_path) + 1));
+	sprintf(path, "%s%s", mount_path, self_path);
+	strcpy(memcg_path, path);
+	return memcg_path;
+}
+
+int open_event_fd(char *memcg_path) {
+	int efd, ecfd, oomfd;
+	char ecfd_str[32];
+	char *filename;
+	ssize_t s;
+	filename = malloc(sizeof(char) * (strlen(memcg_path) + 22));
+	sprintf(filename, "%s/memory.oom_control", memcg_path);
+	oomfd = open(filename, O_RDONLY);
+	if(oomfd == -1)
+		handle_error("open oomfd");
+
+	efd = eventfd(0, 0);
+	if(efd == -1)
+		handle_error("open efd");
+
+	sprintf(filename, "%s/cgroup.event_control", memcg_path);
+	ecfd = open(filename, O_WRONLY);
+	if(ecfd == -1)
+		handle_error("open ecfd");
+
+	free(filename);
+
+	/* configure event_control with file descriptors */
+	sprintf(ecfd_str, "%d %d", efd, oomfd);
+	s = write(ecfd, &ecfd_str, strlen(ecfd_str));
+	if(s != strlen(ecfd_str))
+		handle_error("writing to cgroup.event_control");
+
+	return efd;
+}
+
+void usage(char *progname) {
+	printf(	"usage: %s <path to memory cgroup directory>\n\n"
+		"The path is optional. If none is provided, the current "
+		"memory cgroup will be used. An invalid cgroup or path "
+		"will result in a silent failure.\n", progname );
 }
 
 int main(int argc, char *argv[]) {
-	int efd, ecfd, oomfd;
+	int efd;
 	uint64_t u;
 	ssize_t s;
-	char ecfd_str[32];
 	uid_t uid;
-	struct passwd pwd;
-	char username[32];
-	char filename[255];
-	FILE *write_pipe;
 	struct timeval timeout;
 	int retval;
 	fd_set set;
-	char memcg_path[255];
+	char *memcg_path;
+
+	if(argc > 2) {
+		usage(argv[0]);
+		exit(1);
+	} else if(argc==2 && argv[1][0] == '-') {
+		/* any attempt at -helpme, etc */
+		usage(argv[0]);
+		exit(0);
+	}
+	
 
 	uid = getuid();
 	/* don't monitor root */
@@ -288,54 +317,44 @@ int main(int argc, char *argv[]) {
 		exit(0);
 	if(findDuplicate(argv[0], uid))
 		exit(0);
+
 	daemonize();
-	getUsernameFromUid(uid, username);	
 
-	get_cgroup_path(memcg_path);
+	if(argc == 2)
+		memcg_path = argv[1];
+	else
+		memcg_path = get_cgroup_path();
 
-	sprintf(filename, "%s/memory.oom_control", memcg_path);
+	efd = open_event_fd(memcg_path);
 
-
-	oomfd = open(filename, O_RDONLY);
-	if(oomfd == -1)
-		handle_error("oomfd");
-
-	efd = eventfd(0, 0);
-	if(efd == -1)
-		handle_error("efd");
-
-	sprintf(filename, "%s/cgroup.event_control", memcg_path);
-	ecfd = open(filename, O_WRONLY);
-	if(ecfd == -1)
-		handle_error("ecfd");
-
-	sprintf(ecfd_str, "%d %d", efd, oomfd);
-	s = write(ecfd, &ecfd_str, strlen(ecfd_str));
-	if(s != strlen(ecfd_str))
-		handle_error("writing to cgroup.event_control");
-
-
+	if(argc != 2)
+		free(memcg_path);
 	while(1) {
 		do {
-			timeout.tv_sec = 30;
+			/* Check periodically if the user has other processes.
+			   Exit if none exist */
+			timeout.tv_sec = TIMEOUT_SECONDS;
 			timeout.tv_usec = 0;
-
 			FD_ZERO(&set);
 			FD_SET(efd, &set);
+
+			/* check for data in efd (i.e. oom condition triggered)  */
 			retval = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
 			if(retval == 0) {
+				/* select() timed out */
 				if(userHasOtherProcesses(uid)==0)
 					exit(0);
 			} else {
+				/* select() found data or it failed */
 				s = read(efd, &u, sizeof(uint64_t));
 			}
 		} while(retval < 1);
 		if (s != sizeof(uint64_t))
-			handle_error("read");
+			handle_error("reading from event fd");
 
 		/* Wait a moment for the oom-killer to take effect. */
-		sleep(2);
-		writeToActiveTty(uid);
+		sleep(4);
+		writeToUserTTYs(uid);
 	}
 	exit(0);
 }
